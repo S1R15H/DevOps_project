0dba1917700ee9c64370a1d35eba83fa9a3f5e3c
Sirish Gurung <sirishgurung112@gmail.com>
2026-07-24 12:48:08 -0400

    Added testing

--- src/app.js
+++ src/app.js
@@ -32,4 +32,8 @@ app.get('/api', (req, res) => {
 app.use('/api/auth',authRoutes);
 app.use('/api/users', usersRoutes);
 
+app.use((req,res)=>{
+  res.status(404).json({ error: 'Not Found', message: 'The requested resource was not found on this server.' });
+})
+
 export default app;
